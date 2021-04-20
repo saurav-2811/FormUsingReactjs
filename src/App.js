7404bd401686a6f7e1fa1580af7e3603b0e90052
saurav2811 <saurav.sri2811@gmail.com>
2021-04-21 02:28:52 +0530

    use our json server, fetch data from server and now able to add ,update and delete both side ui and server

--- src/App.js
+++ src/App.js
@@ -1,45 +1,71 @@
-import {useState} from 'react'
+import {useState ,useEffect} from 'react'
 import Header from './Components/Header'
 import AddTask from './Components/AddTask'
 import Tasks from './Components/Tasks'
 function App() {
-  const[tasks,setTasks]=useState([
-    {
-    id:1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true   
-},
-{
-    id:2,
-    text: "Doctor Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: false  
-},
-{
-    id:3,
-    text: "Doct Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true   
-}
-])  
-
+  const[tasks,setTasks]=useState([])  
+  useEffect(()=>{
+    const getData=async()=>{
+      const taskFromServer= await fetchApi()
+      setTasks(taskFromServer)
+    }
+    getData()
+  },[])
+  const fetchApi=async()=>{
+    let res=await fetch('http://localhost:5000/tasks')
+    let data= await res.json()
+    return data
+  }
+  const fetchTask=async(id)=>{
+    let res=await fetch(`http://localhost:5000/tasks/${id}`)
+    let data= await res.json()
+    return data
+  }
   const [showAddTask , setShowAddTask]=useState(false)
-  const addTask=(task)=>{
-    const id= Math.floor(Math.random()*10000)+1
-    const newTask={id , ...task}
-    setTasks([...tasks ,newTask])
+  const addTask=async(task)=>{
+    const res=await fetch('http://localhost:5000/tasks',
+    {
+      method:'POST',
+    headers:{
+      'Content-type':'application/json'
+    },
+    body:JSON.stringify(task)
+  }
+    )
+    const data=await res.json()
+    setTasks([...tasks, data])
+    // const id= Math.floor(Math.random()*10000)+1
+    // const newTask={id , ...task}
+    // setTasks([...tasks ,newTask])
   }
 
 
-  const deleteTask= (id)=>{
+  const deleteTask= async(id)=>{
+    await fetch(`http://localhost:5000/tasks/${id}`,
+    {
+      method:'DELETE'
+    })
+
    setTasks(tasks.filter((task)=>(
                task.id!==id
    )
     ))
   }
 
-const toggleReminder=(id)=>{
+const toggleReminder=async(id)=>{
+    const taskToggle=await fetchTask(id)
+    const updTask={...taskToggle, reminder:!taskToggle.reminder}
+    const res= await fetch(`http://localhost:5000/tasks/${id}`,
+    {
+      method:'PUT',
+    headers:{
+      'Content-type':'application/json'
+    },
+    body:JSON.stringify(updTask)
+  }
+    )
+    const data=await res.json()
+
  setTasks(tasks.map((task)=>(
    task.id===id?{ ...task ,reminder:!task.reminder}:task
   )
