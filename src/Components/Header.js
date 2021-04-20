777a701b51bf37df2417bf547cb110d7f3b810b2
saurav2811 <saurav.sri2811@gmail.com>
2021-04-21 03:10:07 +0530

    prevent add button displaying on other routes use react-router-dom {useLocation}

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
+import {useLocation} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Button from './Button'
 const Header=({title , onAdd , showTask})=>{
+    const Location=useLocation()
     return(
         <header className="header">
             <h1>{title}</h1>
-            <Button color={showTask?"red":"green"}onClick={onAdd} text={showTask?"close":"add"}/>
+            {Location.pathname==='/'?<Button color={showTask?"red":"green"}onClick={onAdd} text={showTask?"close":"add"}/>:""}
         </header>
     )
 }
